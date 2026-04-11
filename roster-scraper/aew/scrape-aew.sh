e3685a8e4510c3c63d81943699fb759f376b8f48
Alex McAuliffe <alex@romanx.co.uk>
2026-04-11 13:58:14 +0100

    Convert AEW/TNA/WWE Scrapers to Playwright
    This outgrew shot-scraper and I needed more control

--- roster-scraper/aew/scrape-aew.sh
+++ roster-scraper/aew/scrape-aew.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-bun build "$SCRIPT_DIR/aew.ts" > "$SCRIPT_DIR/aew.js"
-
-shot-scraper javascript -i "$SCRIPT_DIR/aew.js" https://www.allelitewrestling.com/aew-roster --raw | jq -f "$SCRIPT_DIR/order.jq" > "$SCRIPT_DIR/../../rosters/aew-roster.json"
+npx tsx "$SCRIPT_DIR/scrape-aew.ts"
--- roster-scraper/wwe/scrape-wwe.sh
+++ roster-scraper/wwe/scrape-wwe.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-bun build "$SCRIPT_DIR/wwe.ts" > "$SCRIPT_DIR/wwe.js"
-
-shot-scraper javascript -i "$SCRIPT_DIR/wwe.js" https://www.wwe.com/superstars --raw | jq -f "$SCRIPT_DIR/order.jq" > "$SCRIPT_DIR/../../rosters/wwe-roster.json"
+npx tsx "$SCRIPT_DIR/scrape-wwe.ts"
